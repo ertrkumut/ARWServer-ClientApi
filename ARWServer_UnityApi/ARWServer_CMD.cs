02ced84201b749adfe97d2f5ad60c132ee73d24e
Umutcan Erturk <powerLEd@Umutcans-MacBook-Pro.local>
2017-03-02 16:21:26 +0200

    Server request commands

---
+++ ARWServer_UnityApi/ARWServer_CMD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ARWServer
+{
+	public static class ARWServer_CMD
+	{
+		public static string Connection_Success = "CONNECTIONSUCCESS_EVENT";
+		public static string Login              = "LOGIN_EVENT";
+		public static string Join_Room          = "ROOM_JOIN";
+		public static string User_Enter_Room    = "USER_ENTER_ROOM";
+		public static string User_Exit_Room     = "USER_EXIT_ROOM";
+	}
+}
+
