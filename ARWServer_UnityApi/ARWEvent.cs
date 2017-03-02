6e1845630957d323b31818ca563dd1d390f9f315
Umutcan Erturk <powerLEd@Umutcans-MacBook-Pro.local>
2017-03-02 16:20:23 +0200

    New Events and Initialize Events

--- ARWServer_UnityApi/ARWEvent.cs
+++ ARWServer_UnityApi/ARWEvent.cs
@@ -8,7 +8,17 @@ namespace ARWServer
 	public class ARWEvent
 	{
 		public EventHandler handler;
+		public string eventName;
 
+		public ARWEvent(){
+			eventName = String.Empty;
+			handler = null;
+		}
+
+		public ARWEvent(string eventName){
+			this.eventName = eventName;
+			handler = null;
+		}
 	}
 }
 
--- ARWServer_UnityApi/ARWEvents.cs
+++ ARWServer_UnityApi/ARWEvents.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ARWServer
 {
 	public class ARWEvents
 	{
+		public static List<ARWEvent> allEvents;
+
 		public static ARWEvent CONNECTION;
 		public static ARWEvent CONNECTION_LOST;
 		public static ARWEvent EXTENSION_RESPONCE;
@@ -15,17 +18,26 @@ namespace ARWServer
 		public static ARWEvent USER_ENTER_ROOM;
 		public static ARWEvent USER_EXIT_ROOM;
 
+
 		public static void Init(){
-			CONNECTION 			= new ARWEvent();
+			allEvents = new List<ARWEvent> ();
+
+			CONNECTION 			= new ARWEvent(ARWServer_CMD.Connection_Success);
 			CONNECTION_LOST 	= new ARWEvent ();
 			EXTENSION_RESPONCE 	= new ARWEvent ();
-			LOGIN 				= new ARWEvent ();
+			LOGIN 				= new ARWEvent (ARWServer_CMD.Login);
 			LOGIN_ERROR 		= new ARWEvent ();
 			LOGOUT 				= new ARWEvent ();
-			ROOM_JOIN 			= new ARWEvent ();
+			ROOM_JOIN 			= new ARWEvent (ARWServer_CMD.Join_Room);
 			ROOM_JOIN_ERROR 	= new ARWEvent ();
-			USER_ENTER_ROOM 	= new ARWEvent ();
-			USER_EXIT_ROOM 		= new ARWEvent ();
+			USER_ENTER_ROOM 	= new ARWEvent (ARWServer_CMD.User_Enter_Room);
+			USER_EXIT_ROOM 		= new ARWEvent (ARWServer_CMD.User_Exit_Room);
+
+			allEvents.Add (CONNECTION);
+			allEvents.Add (LOGIN);
+			allEvents.Add (ROOM_JOIN);
+			allEvents.Add (USER_ENTER_ROOM);
+			allEvents.Add (USER_EXIT_ROOM);
 		}
 	}
 }
