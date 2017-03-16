51f6a9f5a2cccda4997b5cdebf2ba58902f725ca
Umutcan Erturk <powerLEd@Umutcans-MacBook-Pro.local>
2017-03-16 14:57:50 +0200

    deleted pragma

--- Assets/Plugin/ARWServer/ARWObject.cs
+++ Assets/Plugin/ARWServer/ARWObject.cs
@@ -2,10 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 
-#pragma warning disable 0168 // variable declared but not used.
-#pragma warning disable 0219 // variable assigned but not used.
-#pragma warning disable 0414 // private field assigned but not used.
-
 namespace ARWServer_UnityApi
 {
 
--- Assets/Plugin/ARWServer/ARWServer.cs
+++ Assets/Plugin/ARWServer/ARWServer.cs
@@ -5,10 +5,6 @@ using System.Net;
 using System.Threading;
 using System.Linq;
 
-#pragma warning disable 0168 // variable declared but not used.
-#pragma warning disable 0219 // variable assigned but not used.
-#pragma warning disable 0414 // private field assigned but not used.
-
 namespace ARWServer_UnityApi
 {
 
