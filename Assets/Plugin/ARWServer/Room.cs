8f4717a6bb28b5de4cd746e9b8e4f75a01c717c4
Umut <u.c.erturk@gmail.com>
2017-04-21 01:09:39 +0300

    Room create Update

--- Assets/Plugin/ARWServer/Room.cs
+++ Assets/Plugin/ARWServer/Room.cs
@@ -15,11 +15,14 @@ namespace ARWServer_UnityApi
 				this.tag = e.GetString ("RoomTag");
 
 				userList = new User[e.GetInt ("RoomCappacity")];
+				string[] userArray = e.GetString("Users").Split(new string[] {"''"}, StringSplitOptions.None);
+				for(int ii = 0; ii< userArray.Length; ii++){
+					string[] userDataParts = userArray[ii].Split(new string[]{"^^"}, StringSplitOptions.None);
+					User u = new User(userDataParts[0], int.Parse(userDataParts[1]), bool.Parse(userDataParts[2]));
+				}
 			}catch(System.NullReferenceException){
-
-			}
+			}catch(System.IndexOutOfRangeException){}
 		}
-
 	}
 }
 
