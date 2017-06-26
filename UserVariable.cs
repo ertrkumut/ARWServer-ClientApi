2792b95a32bf9a107c825c45f4ec023f24dfff42
ertrkumut <u.c.erturk@gmail.com>
2017-06-26 22:08:00 +0300

    User Variable

---
+++ UserVariable.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+namespace ARWServer_UnityApi{
+	public class UserVariable{
+		public string key;
+		private object value;
+
+		public UserVariable(string key, object value){
+			this.key = key;
+			this.value = value;
+		}
+
+		public string GetStringValue(){
+			return this.value.ToString();
+		}
+
+		public int GetIntValue(){
+			return int.Parse(this.value.ToString());
+		}
+
+		public int GetFloatValue(){
+			return float.Parse(this.value.ToString());
+		}
+
+		public int GetBoolValue(){
+			return bool.Parse(this.value.ToString());
+		}
+	}	
+}
